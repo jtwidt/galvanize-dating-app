459f871d59297f22b5f4bba41314cb410744231e
Jason Taylor <taylorjason@users.noreply.github.com>
2020-07-07 16:23:58 -0400

    basic react router implementation. switches component based on url/nav bar item clicked

--- public/src/App.js
+++ public/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import SignUp from "./components/SignUp";
 import Profile from "./components/Profile";
@@ -6,13 +7,40 @@ import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <hr />
-      <SignUp />
-      <hr />
-      <Profile />
-    </div>
+    <Router>
+      <div className="App">
+        <div className="ui secondary  menu">
+          <div className="item">
+            <Link to="/signup">Home</Link>
+          </div>
+          <div className="right menu">
+            {/* <div className="item">
+            <div className="ui icon input">
+              <input type="text" placeholder="Search..." />
+              <i className="search link icon"></i>
+            </div>
+          </div> */}
+            <a className="item">
+              <Link to="/profile">Profile</Link>
+            </a>
+            <a className="ui item">
+              <Link to="/login">Login</Link>
+            </a>
+          </div>
+        </div>
+      </div>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
