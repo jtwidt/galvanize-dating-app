d1aacb22fa2c4e02b424e36ad723e9ece83eed4a
Jason Taylor <taylorjason@users.noreply.github.com>
2020-07-07 13:25:01 -0400

    Add semantic UI css via CDN. Not using React Semantic UI currently. Add SignUp component. No logic

---
+++ public/src/components/SignUp.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useState } from "react";
+
+function SignUp() {
+  const [user, setUser] = useState({});
+
+  return (
+    <div className="ui centered grid">
+      <div className="eight wide column">
+        <div className="ui container">
+          <h1 className="ui center aligned icon header">
+            <i className="lock icon"></i>Sign Up
+          </h1>
+          <form className="ui form">
+            <div className="field">
+              <label>First Name</label>
+              <input type="text" name="first_name" value="First Name"></input>
+            </div>{" "}
+            <div className="field">
+              <label>Last Name</label>
+              <input type="text"></input>{" "}
+            </div>{" "}
+            <div className="field">
+              <label>Display Name</label>
+              <input type="text"></input>{" "}
+            </div>{" "}
+            <div className="field">
+              <label>Email</label>
+              <input type="text"></input>{" "}
+            </div>{" "}
+            <div className="field">
+              <label>Password</label>
+              <input type="text"></input>{" "}
+            </div>{" "}
+            <div className="field">
+              <label>Verify Password</label>
+              <input type="text"></input>{" "}
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
