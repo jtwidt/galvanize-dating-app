7d2fdc38e2b200fa0e88e41f20282bf91833246b
Jason Taylor <taylorjason@users.noreply.github.com>
2020-07-09 00:32:29 -0400

    NEW: Add signup functionality
    Add auto-complete, success message and loading message in addition to core functionality requirements

--- public/src/components/SignUp.js
+++ public/src/components/SignUp.js
@@ -1,8 +1,46 @@
 import React from "react";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import md5 from "js-md5";
+
+const init = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  display_name: "",
+  password1: "",
+  password2: "",
+};
 
 function SignUp() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(init);
+  const [loading, setLoading] = useState(false);
+  const [created, setCreated] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const history = useHistory();
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    if (user.password1 !== user.password2) {
+      console.log("Password's do not match!");
+      return;
+    }
+    fetch(`http://localhost:3010/api/v1/users/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ...user, password: md5(user.password1) }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        setLoading(false);
+        if ((json = "Account created")) {
+          setCreated(true);
+          setDisabled(true);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="ui centered grid">
@@ -11,32 +49,105 @@ function SignUp() {
           <h1 className="ui center aligned icon header">
             <i className="user plus icon"></i>Sign Up
           </h1>
-          <form className="ui form">
-            <div className="field">
+          <form
+            className={`ui ${created ? "success" : ""} form`}
+            onSubmit={(event) => onFormSubmit(event)}
+          >
+            <div className={`${disabled ? "disabled" : ""} field`}>
               <label>First Name</label>
-              <input type="text" name="first_name" value=""></input>
+              <input
+                type="text"
+                name="first_name"
+                value={user.first_name}
+                onChange={(e) =>
+                  setUser({
+                    ...user,
+                    first_name: e.target.value,
+                    display_name: `${e.target.value} ${user.last_name}`,
+                  })
+                }
+              ></input>
             </div>{" "}
-            <div className="field">
+            <div className={`${disabled ? "disabled" : ""} field`}>
               <label>Last Name</label>
-              <input type="text"></input>{" "}
+              <input
+                type="text"
+                value={user.last_name}
+                onChange={(e) =>
+                  setUser({
+                    ...user,
+                    last_name: e.target.value,
+                    display_name: `${user.first_name} ${e.target.value}`,
+                  })
+                }
+              ></input>{" "}
             </div>{" "}
-            <div className="field">
+            <div className={`${disabled ? "disabled" : ""} field`}>
               <label>Display Name</label>
-              <input type="text"></input>{" "}
+              <input
+                type="text"
+                value={user.display_name}
+                onChange={(e) =>
+                  setUser({ ...user, display_name: e.target.value })
+                }
+              ></input>{" "}
             </div>{" "}
-            <div className="field">
+            <div className={`${disabled ? "disabled" : ""} field`}>
               <label>Email</label>
-              <input type="text"></input>{" "}
+              <input
+                type="text"
+                value={user.email}
+                autoComplete="email username"
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+              ></input>{" "}
             </div>{" "}
-            <div className="field">
+            <div className={`${disabled ? "disabled" : ""} field`}>
               <label>Password</label>
-              <input type="text"></input>{" "}
+              <input
+                type="password"
+                value={user.password1}
+                autoComplete="new-password"
+                onChange={(e) =>
+                  setUser({ ...user, password1: e.target.value })
+                }
+              ></input>{" "}
             </div>{" "}
-            <div className="field">
+            <div className={`${disabled ? "disabled" : ""} field`}>
               <label>Verify Password</label>
-              <input type="text"></input>{" "}
+              <input
+                type="password"
+                value={user.password2}
+                autoComplete="new-password"
+                onChange={(e) =>
+                  setUser({ ...user, password2: e.target.value })
+                }
+              ></input>{" "}
             </div>
-            <button class="ui right floated primary button">
+            <div className={`ui ${loading ? "" : "hidden"} icon message`}>
+              <i className="notched circle loading icon"></i>
+              <div className="content">
+                <div className="header">Just one second</div>
+                <p>We're creating that account for you.</p>
+              </div>
+            </div>
+            <div className={`ui success message`}>
+              {/* <i className="close icon"></i> */}
+              <div className="header">
+                Your user registration was successful.
+              </div>
+              <button
+                className="ui right floated compact primary button"
+                onClick={() => history.push("/login")}
+              >
+                Login
+              </button>
+              <p>You may now log-in with the username you have chosen</p>
+            </div>
+            <button
+              className={`ui ${
+                disabled ? "disabled" : ""
+              } right floated primary button`}
+            >
               Create Account
             </button>
           </form>
